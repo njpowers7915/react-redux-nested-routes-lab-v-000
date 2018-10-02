90744bfa68a35d5974bc1ae04e32cb6b806fb468
njpowers7915 <njpowers7915@gmail.com>
2018-10-02 14:21:33 -0400

    Done.

--- test/PetsNewTest.js
+++ test/PetsNewTest.js
@@ -4,10 +4,3 @@ import { mount } from 'enzyme';
 import store from '../src/configureStore';
 import FakeProvider from './FakeProvider';
 import PetsNew from '../src/containers/PetsNew';
-
-describe('PetsNew', () => {
-  it('renders the form to create a new Pet', () => {
-    const wrapper = mount(<FakeProvider><PetsNew /></FakeProvider>);
-    expect(wrapper.find('form').length).toEqual(1, 'Pets New Component should contain a form');
-  })
-})
